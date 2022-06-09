ac274c1ec9cf990ac10ddb33459a8e263d28e95a
shannonasor <shannonreed@asordevelopment.com>
2022-06-09 00:26:45 -0700

    finished left side draft

--- src/App.js
+++ src/App.js
@@ -2,66 +2,55 @@ import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedinIn, faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faHandHoldingDollar, faHouse, faNewspaper, faReceipt, faTableList } from '@fortawesome/free-solid-svg-icons'
 import example from './example.jpg'
 
-import Section2 from './secondsection/Section2';
-import Footer from './footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className='Header'>
-        <h1 className='Org-Name'>Big D's House</h1>
-        <div className='Menu'>
-            <ul className='Main-Menu'>
-              <li>Home</li>
-              <li>Resources</li>
-              <li>News  Events</li>
-              <li>Donate</li>
-              <li>Orders</li>
-            </ul>
+      <div className='Left'>
+          <div className='Left-Main-Heading'>
+            <h1>Big D's House</h1>
+          </div>
+          <div className='Left-Main-Menu'>
+            <ul className= 'Left-Main-Menu-Icon-List'>
+                <li><FontAwesomeIcon icon={faHouse} /></li>
+                <li><FontAwesomeIcon icon={faTableList} /></li>
+                <li><FontAwesomeIcon icon={faNewspaper} /></li>
+                <li><FontAwesomeIcon icon={faHandHoldingDollar} /></li>
+                <li><FontAwesomeIcon icon={faReceipt} /></li>
+              </ul>
 
-            <ul className='Social-Menu'>
+              <ul className= 'Left-Main-Menu-List'>
+                <li>Home</li>
+                <li>Resources</li>
+                <li>News  Events</li>
+                <li>Donate</li>
+                <li>Orders</li>
+              </ul>
+          </div>
+          <div className='Left-Social-Menu'>
+          <ul className='Left-Social-Menu-List'>
               <li><FontAwesomeIcon icon={faFacebook} /></li>
               <li><FontAwesomeIcon icon={faLinkedinIn} /></li>
               <li><FontAwesomeIcon icon={faInstagram} /></li>
               <li><FontAwesomeIcon icon={faTwitter} /></li>
               <li><FontAwesomeIcon icon={faYoutube} /></li>
             </ul>
-        </div>
-      </header>
-
-
-      <div className='container'>
-        <div className='front'>
-          <div className='front-one'>
-            <img className= 'top-img' src={example} alt="world" />
-            <h3 className='h3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-            <h4 className='h4'> Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h4>
-            <button class="button-1">Learn More</button>
+            
           </div>
-
-          <div className='front-two'>
-            <ul className='Info-Menu'>
-                <li>
-                  <h3>Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h3>
-                  <h4>Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h4>
-                </li>
-                <li>
-                  <h3>Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h3>
-                  <h4>Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h4>
-                </li>
-                <li>
-                  <h3>Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h3>
-                  <h4>Etiam ultrices sit amet nunc a consequat. Nullam a orci venenatis, accumsan erat vitae, posuere libero.</h4>
-                </li>
-              </ul>
-          </div>
-        </div>
       </div>
 
-      <Section2 />
-      <Footer />
+      <div className='Right'>
+          <div className='Top-Right'>
+            <img src={example} alt="world" />
+          </div>
+          <div>
+            
+          </div>
+
+      </div>
     </div>
   );
 }
